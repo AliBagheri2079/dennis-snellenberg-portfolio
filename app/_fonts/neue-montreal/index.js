894937ab61445b396c610244caac2b8a5e1403f9
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-18 15:28:09 +0330

    :lipstick: Style: add neue montreal font
    add neue montreal font config with multiple format

---
+++ app/_fonts/neue-montreal/index.js
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+export const neue_montreal = localFont({
+  src: './regular/index.woff2',
+  weight: 'normal',
+  style: 'normal',
+  variable: '--font-neue-montreal',
+  display: 'swap',
+});
