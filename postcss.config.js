4a2d758a2e80d81884d0ab3b4a63f3cb9f05f4b0
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-18 14:13:24 +0330

    :wrench: Chore: update postcss config
    add postcss import, tailwindcss nesting, postcss preset envirement and cssnano configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
     autoprefixer: {},
+    cssnano: { preset: 'default' },
   },
-}
+};
