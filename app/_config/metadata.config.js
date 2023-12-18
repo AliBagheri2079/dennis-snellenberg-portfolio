4b9a35507ade56dee2efc1f050e25088ebf628b6
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-18 16:15:20 +0330

    :art: Refactor: create root metadata config
    create root metadata config file with metadata options

---
+++ app/_config/metadata.config.js
@@ -0,0 +1,17 @@
+export const rootMetadata = {
+  title: {
+    template: '%s | Dennis Snellenberg',
+    default: 'Dennis Snellenberg • Freelance Designer & Developer',
+  },
+  description:
+    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+  generator: 'Dennis Snellenberg',
+  applicationName: 'Dennis Snellenberg',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Design', 'Develope', 'Freelance'],
+  authors: [
+    { name: 'Ali Bagheri', url: 'https://www.github.com/alibagheri2079' },
+  ],
+  creator: 'Ali Bagheri',
+  publisher: 'Ali Bagheri',
+};
