70f6bdf8c8ceaa2b6f7e14a5447fe531770788a2
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-19 17:50:20 +0330

    :sparkles: Feat: create parallax slider component
    create parallax slider component with useParallaxSlider hook

---
+++ app/_components/parallax/slider/index.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Fragment } from 'react';
+import { motion } from 'framer-motion';
+
+import { randomId } from '@/app/_utils';
+import { useParallaxSlider } from '@/app/_hooks';
+
+/** @param {import('react').PropsWithChildren<unknown> & {repeat: number; baseVelocity: number;}} */
+export function ParallaxSlider({ children, repeat = 2, baseVelocity }) {
+  const x = useParallaxSlider(baseVelocity);
+
+  return (
+    <div className='flex flex-nowrap overflow-hidden whitespace-nowrap'>
+      <motion.div style={{ x }}>
+        {Array.from({ length: repeat }, () => {
+          const id = randomId();
+          return <Fragment key={id}>{children}</Fragment>;
+        })}
+      </motion.div>
+    </div>
+  );
+}
