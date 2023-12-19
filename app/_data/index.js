0f6c97daf0c838e424a2f63eaaf97132644eb22b
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 01:32:00 +0330

    :sparkles: Feat: create social medias data
    create social medias data with real url

--- app/_data/index.js
+++ app/_data/index.js
@@ -1,3 +1,4 @@
 export * from './nav-items';
 export * from './project-options';
+export * from './social-medias';
 export * from './thumbnail-options';
---
+++ app/_data/social-medias.js
@@ -0,0 +1,18 @@
+export const socialMedias = [
+  {
+    href: 'https://www.awwwards.com/dennissnellenberg/',
+    title: 'Awwwards',
+  },
+  {
+    href: 'https://www.instagram.com/codebydennis/',
+    title: 'Instagram',
+  },
+  {
+    href: 'https://dribbble.com/dennissnellenberg',
+    title: 'Dribbble',
+  },
+  {
+    href: 'https://www.linkedin.com/in/dennissnellenberg/',
+    title: 'LinkedIn',
+  },
+];
