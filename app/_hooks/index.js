1694c93a37c797a5557bbaa72d71ae5f5bdb2cc7
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 00:05:37 +0330

    :sparkles: Feat: create project slider hook
    create project slider custom hook for working project layout

--- app/_hooks/index.js
+++ app/_hooks/index.js
@@ -1,3 +1,4 @@
 export * from './use-follow-pointer';
 export * from './use-magnetic';
 export * from './use-parallax-slider';
+export * from './use-project-slider';
---
+++ app/_hooks/use-project-slider.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { useScroll, useTransform } from 'framer-motion';
+
+/** @param {import('react').MutableRefObject<HTMLElement>} element */
+export function useProjectSlider(element) {
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['start end', 'end start'],
+  });
+
+  const transformX1 = useTransform(scrollYProgress, [0, 1], [0, -250]);
+  const transformX2 = useTransform(scrollYProgress, [0, 1], [-250, 0]);
+  const transformY = useTransform(scrollYProgress, [0, 0.9], [400, 0]);
+
+  return { transformX1, transformX2, transformY };
+}
