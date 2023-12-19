a8d1b5582304f46b87204043f2c3b3b98fc4ea10
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-19 15:25:17 +0330

    :sparkles: Feat: create magnetic hook
    create custom magnetic hook for working with magnetic button

---
+++ app/_hooks/use-magnetic.js
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState, useCallback } from 'react';
+
+/** @param {import('react').MutableRefObject<HTMLButtonElement>} element */
+export function useMagnetic(element) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  /** @type {import('react').PointerEventHandler<HTMLButtonElement>} */
+  const handleMagneticMove = useCallback(
+    event => {
+      const { clientX, clientY } = event;
+      const { width, height, left, top } =
+        element.current.getBoundingClientRect();
+
+      const x = (clientX - (left + width / 2)) * 0.35;
+      const y = (clientY - (top + height / 2)) * 0.35;
+      setPosition(prePosition => ({ ...prePosition, x, y }));
+    },
+    [element],
+  );
+
+  const handleMagneticOut = useCallback(() => setPosition({ x: 0, y: 0 }), []);
+
+  return { position, handleMagneticMove, handleMagneticOut };
+}
