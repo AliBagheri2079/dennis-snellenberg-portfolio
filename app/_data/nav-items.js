27d4d2fd34a30c270ec8b89036e0096f1c3ee025
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-19 16:45:14 +0330

    :zap: Perf: improve performance with create nav items data
    create nav items data with custom options

---
+++ app/_data/nav-items.js
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    href: '/',
+    title: 'home',
+  },
+  {
+    href: '/work',
+    title: 'work',
+  },
+  {
+    href: '/about',
+    title: 'about',
+  },
+  {
+    href: '/contact',
+    title: 'contact',
+  },
+];
