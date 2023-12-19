4f6e2750430d01238dee88a6c66ecbdaa0cdf03a
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-19 22:20:09 +0330

    :sparkles: Feat: add thumbnail option
    create thumbnail option data file

--- app/_data/index.js
+++ app/_data/index.js
@@ -1,1 +1,2 @@
 export * from './nav-items';
+export * from './thumbnail-options';
---
+++ app/_data/thumbnail-options.js
@@ -0,0 +1,22 @@
+export const thumbnailOptions = [
+  {
+    href: '/fabric',
+    title: 'FABRIC™',
+    image: 'Dennis-Portfolio/images/hlz36eeb3ddfvqt9tex9',
+  },
+  {
+    href: '/aanstekelijk',
+    title: 'Aanstekelijk',
+    image: 'Dennis-Portfolio/images/aigt2q3xiqb8ovujzscc',
+  },
+  {
+    href: '/base-create',
+    title: 'Base Create',
+    image: 'Dennis-Portfolio/images/ahuzarh793qmv9sdveyk',
+  },
+  {
+    href: '/avvr',
+    title: 'AVVR',
+    image: 'Dennis-Portfolio/images/pzychyr1ans60gqlevhg',
+  },
+];
