29a028f1ec24f5d0749348b9188fae120316b6eb
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 01:29:46 +0330

    :sparkles: Feat: create contact slider hook
    create contact slider custom hook for working with contact layout

--- app/_hooks/index.js
+++ app/_hooks/index.js
@@ -1,3 +1,4 @@
+export * from './use-contact-slider';
 export * from './use-follow-pointer';
 export * from './use-magnetic';
 export * from './use-parallax-slider';
---
+++ app/_hooks/use-contact-slider.js
@@ -0,0 +1,16 @@
+'use client';
+
+import { useScroll, useTransform } from 'framer-motion';
+
+/** @param {import('react').MutableRefObject<HTMLElement>} element */
+export function useContactSlider(element) {
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['start end', 'end start'],
+  });
+
+  const transformX = useTransform(scrollYProgress, [0, 1], [-50, 50]);
+  const transformY = useTransform(scrollYProgress, [0, 1], [-400, 0]);
+
+  return { transformX, transformY };
+}
