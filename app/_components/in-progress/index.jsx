4af6e24fcfb870de9f3e7bb148475e04fc39584a
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 23:41:03 +0330

    :sparkles: Feat: create in progress component
    create in progress component for in progress pages

---
+++ app/_components/in-progress/index.jsx
@@ -0,0 +1,19 @@
+import { Center, ParallaxSlider } from '@/app/_components';
+
+/** @param {import('react').PropsWithChildren<unknown>} */
+export function InProgress({ children }) {
+  return (
+    <Center className='mb-80 h-screen'>
+      <div className='select-none'>
+        <h1 className='text-[max(10em,18vw)]'>
+          <ParallaxSlider repeat={8} baseVelocity={2}>
+            <span className='pe-12'>
+              {children}
+              <span className='spacer'>—</span>
+            </span>
+          </ParallaxSlider>
+        </h1>
+      </div>
+    </Center>
+  );
+}
--- app/_components/index.js
+++ app/_components/index.js
@@ -1,3 +1,4 @@
 export * from './common';
+export * from './in-progress';
 export * from './parallax';
 export * from './stack';
