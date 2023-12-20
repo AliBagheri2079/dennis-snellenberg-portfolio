03ef5c51e919cb649b17d11fb8142089eb865e07
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 21:01:47 +0330

    :sparkles: Feat: create offcanvas toggle hook
    create offcanvas toggle custom hook with framer hooks

--- app/_hooks/index.js
+++ app/_hooks/index.js
@@ -1,5 +1,6 @@
 export * from './use-contact-slider';
 export * from './use-follow-pointer';
 export * from './use-magnetic';
+export * from './use-offcanvas-toggle';
 export * from './use-parallax-slider';
 export * from './use-project-slider';
---
+++ app/_hooks/use-offcanvas-toggle.js
@@ -0,0 +1,19 @@
+'use client';
+
+import { useScroll, useMotionValueEvent } from 'framer-motion';
+
+/**
+ * @param {Object} params
+ * @param {import('react').MutableRefObject<HTMLDivElement>} params.element
+ * @param {(latestValue: number) => void} params.callback
+ */
+export function useOffcanvasToggle({ element, callback }) {
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['500px 500px', 'end start'],
+  });
+
+  useMotionValueEvent(scrollYProgress, 'change', callback);
+
+  return { scrollYProgress };
+}
