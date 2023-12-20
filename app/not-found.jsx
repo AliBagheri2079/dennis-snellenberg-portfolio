5a1e06d351ce8bbaccf354ce1430cb1160d99840
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 23:43:48 +0330

    :sparkles: Feat: create not found page
    create custom not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import { InProgress } from './_components';
+
+export const metadata = {
+  title: '404',
+  description:
+    'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+};
+
+export default function NotFound() {
+  return <InProgress>Not Found</InProgress>;
+}
