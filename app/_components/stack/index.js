c0163b9cb8dc871f2cde8c44b3a6d20f91fc1503
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-20 20:59:31 +0330

    :sparkles: Feat: create many overlay styled component
    create fixed and absolute overlay styled components

--- app/_components/stack/index.js
+++ app/_components/stack/index.js
@@ -1,1 +1,2 @@
 export * from './center.styled';
+export * from './overlay.styled';
---
+++ app/_components/stack/overlay.styled.js
@@ -0,0 +1,13 @@
+'use client';
+
+import { styled } from 'styled-components';
+
+export const FixedOverlay = styled.div`
+  position: fixed;
+  inset: 0;
+`;
+
+export const AbsoluteOverlay = styled.div`
+  position: absolute;
+  inset: 0;
+`;
