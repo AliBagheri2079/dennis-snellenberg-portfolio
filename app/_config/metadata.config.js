8e519e9e1a42384fa893763334ded7fa78a4e604
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-12-22 12:53:02 +0330

    :zap: Perf: improve seo performance
    update metadata with screenshot and add robots setting

--- app/_config/metadata.config.js
+++ app/_config/metadata.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').Metadata} */
 export const rootMetadata = {
+  metadataBase: new URL('https://dennis-snellenberg-portfolio.vercel.app/'),
   title: {
     template: '%s | Dennis Snellenberg',
     default: 'Dennis Snellenberg • Freelance Designer & Developer',
@@ -15,4 +16,30 @@ export const rootMetadata = {
   ],
   creator: 'Ali Bagheri',
   publisher: 'Ali Bagheri',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Dennis Snellenberg',
+    description:
+      'Helping brands thrive in the digital world. Located in The Netherlands. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Dennis',
+    siteId: '1467726470533754880',
+    creator: '@AliBagheri2079',
+    creatorId: '1467726470533754880',
+    images: {
+      url: 'https://dennis-snellenberg-portfolio.vercel.app/screenshot.png',
+      alt: 'Portfolio Screenshot',
+    },
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
